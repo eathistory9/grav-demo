00df13ec2d7be350753bea26a2f54e47663ac63b
Sorin <eathistory9@gmail.com>
2025-09-18 02:44:09 +0300

    Clear cache before deploy

--- cache/compiled/files/c3df9138c73399a2cbea4dbcc342753c.yaml.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'C:/Users/Sorin/OneDrive/Desktop/grav-demo/system/config/security.yaml',
-    'modified' => 1758150459,
-    'size' => 631,
-    'data' => NULL
-];
