bed4b4fa58626d8ca0154d04b3918f6692b5d33b
Sorin <eathistory9@gmail.com>
2025-09-18 02:13:13 +0300

    Complete Grav demo for Render

---
+++ cache/compiled/files/c3df9138c73399a2cbea4dbcc342753c.yaml.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'C:/Users/Sorin/OneDrive/Desktop/grav-demo/system/config/security.yaml',
+    'modified' => 1758150459,
+    'size' => 631,
+    'data' => NULL
+];
